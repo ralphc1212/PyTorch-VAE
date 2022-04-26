9a495ef7ff7a57a114125a908a66967b44391459
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-04-26 14:11:58 +0800

    default

--- models/vq_vae.py
+++ models/vq_vae.py
@@ -51,7 +51,7 @@ class VectorQuantizer(nn.Module):
 
         # Add the residue back to the latents
         quantized_latents = latents + (quantized_latents - latents).detach()
-        print(quantized_latents)
+        print(quantized_latents.shape)
         exit()
         return quantized_latents.permute(0, 3, 1, 2).contiguous(), vq_loss  # [B x D x H x W]
 
