4151e7103db2ad9d4628f39c7deb2e43cc6b47fb
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-04-26 16:09:18 +0800

    default

--- models/vq_vae.py
+++ models/vq_vae.py
@@ -205,8 +205,10 @@ class VQVAE(BaseVAE):
 
         recons_loss = F.mse_loss(recons, input)
         kld_weight = kwargs['M_N']
-        
-        loss = recons_loss + self.beta * kld_weight * vq_loss
+
+        # loss = recons_loss + self.beta * kld_weight * vq_loss
+        loss = recons_loss + self.beta * vq_loss
+
         return {'loss': loss,
                 'Reconstruction_Loss': recons_loss,
                 'VQ_Loss':vq_loss}
